c78918a75a083d14f65438eb14486f7e25bd9d2e
chikataga <chikataga@gmail.com>
2020-01-12 16:30:19 +0900

    add site base

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { Bounce, Cubic } from 'gsap/EasePack';
+import $ from 'jquery';
+import { TweenMax, TimelineMax } from 'gsap';
+import ScrollMagic from 'scrollmagic';
+import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
+import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
+import './scss/style.scss';
+
+
