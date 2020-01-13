a583c2a4e761519a657b2c9e12feb0bcd9587956
chikataga <chikataga@gmail.com>
2020-01-13 21:50:14 +0900

    add random slack icons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import { Bounce, Cubic } from 'gsap/EasePack';
+import { Bounce, Cubic, Linear } from 'gsap/EasePack';
 import $ from 'jquery';
 import { TweenMax, TimelineMax } from 'gsap';
 import ScrollMagic from 'scrollmagic';
 import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap';
 import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
 import './scss/style.scss';
+import 'jquery.random-elements';
 
 window.onload=function(){
   const pageWidth = window.innerWidth;
@@ -24,13 +25,32 @@ window.onload=function(){
       let mX = e.pageX;
       let mY = e.pageY;
 
-      TweenMax.to('.wrapper', 0.3, {
+      TweenMax.to('.wrapper', 0.5, {
         x: - (mX / pageWidth * diffWidth),
         y: - (mY / pageHeight * diffHeight),
-        ease: Power3.easeIn,
+        ease: Linear.easeIn,
       });
 
-    }, 500 ) ;
+    }, 100 ) ;
 
   });
+
+  $(".wrapper").randomElements(
+    [
+      '<img src="img/icons/01.png">',
+      '<img src="img/icons/02.png">',
+      '<img src="img/icons/03.png">'
+    ],
+    {
+      className: 'random-content',
+      num: 20,
+      stageWidthExpansion: 0,
+      stageHeightExpansion: 0,
+      width: 80,
+      height: 80,
+      isRandomSize: false,
+      tryCount: 10,
+      adjustment: 0
+    }
+  );
 }
