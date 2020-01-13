e2f9045fa008d8d2930f0d2f1a23a2199f780df0
chikataga <chikataga@gmail.com>
2020-01-13 20:53:26 +0900

    backgroud animation

--- src/index.js
+++ src/index.js
@@ -6,4 +6,31 @@ import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugin
 import 'imports-loader?define=>false!scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
 import './scss/style.scss';
 
+window.onload=function(){
+  const pageWidth = window.innerWidth;
+  const pageHeight = window.innerHeight;
+  const wrapperWidth = $('.wrapper').width();
+  const wrapperHeight = $('.wrapper').height();
+  const diffWidth = wrapperWidth - pageWidth;
+  const diffHeight = wrapperHeight - pageHeight;
 
+  let timeoutId;
+  document.body.addEventListener("mousemove", function(e){
+    if ( timeoutId ) return ;
+
+    timeoutId = setTimeout( function () {
+      timeoutId = 0 ;
+
+      let mX = e.pageX;
+      let mY = e.pageY;
+
+      TweenMax.to('.wrapper', 0.3, {
+        x: - (mX / pageWidth * diffWidth),
+        y: - (mY / pageHeight * diffHeight),
+        ease: Power3.easeIn,
+      });
+
+    }, 500 ) ;
+
+  });
+}
