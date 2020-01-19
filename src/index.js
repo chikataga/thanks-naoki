e5ed57c51a29897e7088ce429a27df3cb6340172
chikataga <chikataga@gmail.com>
2020-01-19 14:56:30 +0900

    modify append thumbnail

--- src/index.js
+++ src/index.js
@@ -36,39 +36,24 @@ window.onload=function(){
 
   });
 
-  const iconArray = Messages.map(item => `<img src='img/icons/${item.user_id}.png' data-name='${item.user_id}'>`);
+  const iconArray = Messages.map(item => `<img src='img/icons/${item.user_id}.png' class='slack-icon' data-name='${item.user_id}'>`);
+  const iconLength = iconArray.length;
 
   $('.wrapper').randomElements(
     [
-      '<img src="img/icons/01.png">',
-      '<img src="img/icons/02.png">',
-      '<img src="img/icons/03.png">'
+      '<img src="img/icons/01.png" class="naoki-image">',
+      '<img src="img/icons/02.png" class="naoki-image">',
+      '<img src="img/icons/03.png" class="naoki-image">',
+      ...iconArray
     ],
     {
-      className: 'naoki-image',
-      num: 10,
+      num: iconLength,
       stageWidthExpansion: 0,
       stageHeightExpansion: 0,
-      width: 180,
-      height: 180,
       isRandomSize: false,
       tryCount: 10,
-      adjustment: 0
-    }
-  );
-
-  $('.wrapper').randomElements(
-    iconArray,
-    {
-      className: 'slack-icon',
-      num: 60,
-      stageWidthExpansion: 0,
-      stageHeightExpansion: 0,
-      width: 70,
-      height: 70,
-      isRandomSize: false,
-      tryCount: 10,
-      adjustment: 0
+      adjustment: 0,
+      sameRatio: true,
     }
   );
 
@@ -80,29 +65,30 @@ window.onload=function(){
 
     const userName = $(this).attr('data-name');
     const messageItem = Messages.filter(item => item.user_id == userName)[0];
-    $('.message-container').append(`<div class="message-inner"><div class="message-detail"><div class="text">${messageItem.message.replace(/[\r\n]+/g, "<br />")}</div><div class="name">${messageItem.team} ${messageItem.user_name}</div></div></div><img src="img/icons/${messageItem.user_id}.png" class="thumbnail">`)
+    $('.message-container').append(`<div class="message-inner"><div class="message-detail"><div class="text">${messageItem.message.replace(/[\r\n]+/g, "<br />")}</div><div class="name"><div class="team">${messageItem.team}</div>${messageItem.user_name}</div></div><img src="img/icons/${messageItem.user_id}.png" class="thumbnail"></div>`)
 
     const timeLine = new TimelineMax();
     timeLine.set(
-    '.message-inner', {
-      scale: 0.7
-    }).set(
       '.thumbnail', {
       scale: 0.7,
       x: -10,
-       }).to(this, 0.1, {
+      }).set('message-detail', {
+        scale: 0.7
+      }).to(this, 0.1, {
         alpha: 0,
         scale: 0,
         x: -10,
         delay: 0.7,
         ease: Power3.easeIn
+      }).to('.message-inner', 0, {
+        alpha: 1,
       }).to('.thumbnail', 0.3, {
-          alpha: 1,
-          scale: 1,
-          x: 0,
-          delay: 0.1,
-          ease: Power3.easeIn
-        }).to('.message-inner', 0.1, {
+        alpha: 1,
+        scale: 1,
+        x: 0,
+        delay: 0.1,
+        ease: Power3.easeIn
+      }).to('.message-detail', 0.1, {
         alpha: 1,
         scale: 1,
         delay: 0.1,
@@ -116,16 +102,18 @@ window.onload=function(){
     '.slack-icon.active', {
       scale: 1.8
     }
-    ).to('.message-inner', 0.1, {
-          alpha: 0,
-          scale: 0.7,
-          delay: 0.1,
-          ease: Power3.easeIn
-        }).to('.thumbnail', 0.3, {
+    ).to('.message-detail', 0.1, {
+        alpha: 0,
+        scale: 0.7,
+        delay: 0.1,
+        ease: Power3.easeIn
+      }).to('.thumbnail', 0.3, {
         alpha: 0,
         scale: 0.7,
         delay: 0.1,
         ease: Power3.easeIn,
+      }).to('message-inner', 0, {
+        alpha: 0,
         onComplete: function(){
           $('.message-container').empty();
           TweenMax.to('.slack-icon.active', 0.3, {
