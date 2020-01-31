bfa4a593ae514232d7e2db30cf1231ed0375f6c7
chikataga <chikataga@gmail.com>
2020-01-31 22:38:18 +0900

    expand content area, show member tooltips, import json messages

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ import Messages from './messages.json';
 window.onload=function(){
   const pageWidth = window.innerWidth;
   const pageHeight = window.innerHeight;
-  const wrapperWidth = $('.wrapper').width();
-  const wrapperHeight = $('.wrapper').height();
+  const wrapperWidth = $('.wrapper').width() + 120;
+  const wrapperHeight = $('.wrapper').height() + 120 + 87 + 48;
   const diffWidth = wrapperWidth - pageWidth;
   const diffHeight = wrapperHeight - pageHeight;
 
@@ -40,22 +40,79 @@ window.onload=function(){
   const iconLength = iconArray.length;
 
   $('.wrapper').randomElements(
-    [
-      '<img src="img/icons/01.png" class="naoki-image">',
-      '<img src="img/icons/02.png" class="naoki-image">',
-      '<img src="img/icons/03.png" class="naoki-image">',
-      ...iconArray
-    ],
+    iconArray,
     {
       num: iconLength,
       stageWidthExpansion: 0,
       stageHeightExpansion: 0,
       isRandomSize: false,
       tryCount: 10,
+      width: 70,
+      height: 70,
       adjustment: 0,
       sameRatio: true,
     }
   );
+
+  $('.wrapper').randomElements(
+    [
+      '<img src="img/naoki_images/ph01.png" class="naoki-image">',
+      '<img src="img/naoki_images/ph02.png" class="naoki-image">',
+      '<img src="img/naoki_images/ph03.png" class="naoki-image">',
+      '<img src="img/naoki_images/ph04.png" class="naoki-image">',
+      '<img src="img/naoki_images/ph05.png" class="naoki-image">',
+      '<img src="img/naoki_images/ph06.png" class="naoki-image">',
+      '<img src="img/naoki_images/ph07.png" class="naoki-image">',
+      '<img src="img/naoki_images/ph08.png" class="naoki-image">',
+      '<img src="img/naoki_images/ph09.png" class="naoki-image">',
+      '<img src="img/naoki_images/ph10.png" class="naoki-image">',
+      '<img src="img/naoki_images/ph11.png" class="naoki-image">',
+      '<img src="img/naoki_images/ph12.png" class="naoki-image">',
+    ],
+    {
+      num: 12,
+      stageWidthExpansion: 0,
+      stageHeightExpansion: 0,
+      isRandomSize: false,
+      tryCount: 20,
+      width: 320,
+      height: 200,
+      adjustment: 0,
+      sameRatio: true,
+    }
+  );
+
+  $(document).on('mouseenter', '.slack-icon', function(ev) {
+    var top = $(this).position().top;
+    var left = $(this).position().left;
+    top = Math.floor(top);
+    left = Math.floor(left);
+    var text = $(this).attr('data-name');
+    $('body').append(
+      '<div id="tooltips-' +
+        text +
+        '" class="tooltips">' +
+        text +
+        '</div>'
+    );
+    var obj = $('#tooltips-' + top + '-' + left);
+    var x = ev.pageX;
+    var y = ev.pageY;
+    obj.css({ top: y + 34, left: x + 14 });
+  });
+
+  $(document).on('mousemove', '.slack-icon', function(ev) {
+    var text = $(this).attr('data-name');
+    var obj = $('#tooltips-' + text);
+    var x = ev.pageX;
+    var y = ev.pageY;
+    obj.css({ top: y + 34, left: x + 14 });
+  });
+
+  $(document).on('mouseleave', '.slack-icon', function(ev) {
+    var text = $(this).attr('data-name');
+    $('#tooltips-' + text).remove();
+  });
 
   $(document).on("click", ".slack-icon", function () {
     if ($('body').hasClass('active')) return;
